1c12d331c4f7eb8368d7286ff798b03994e274ff
Emerson Silva <emrsnwslv@gmail.com>
2021-06-09 10:45:08 -0300

    routes pagecontroller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "dashboard", to: "pages#dashboard"
+  get "user_preferences", to: "pages#user_preferences"
+  
+  resources :movie_picks do
+    patch "refuse", to: "movie_picks#refuse"
+  end
+
 end
