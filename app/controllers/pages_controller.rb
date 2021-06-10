606c65e4a522d967fe517ca28f38d056325c553d
Thais Freire <thaism.freire@outlook.com>
2021-06-10 18:59:06 -0300

    view user_preferences

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,13 @@ class PagesController < ApplicationController
   end
 
   def user_preferences
+    @movies_array = []
+    movies = Movie.all.sample(1)
+    movies.each do |movie|
+      if movie.poster_url != nil
+        @movies_array << movie
+      end
+    end
   end
 
   def profile
