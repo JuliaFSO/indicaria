d207ae0a1c4aa92cd62b6bad5bdd0f6349abc9d0
Emerson Silva <emrsnwslv@gmail.com>
2021-06-10 14:23:43 -0300

    components,controllers, views, routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,17 @@
 class MoviesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show]
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:number_people, :movie_id)
+  end
 end
