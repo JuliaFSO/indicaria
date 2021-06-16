2e71cedbee8710c1367aee7183be92fbfdfd87b2
Thais Freire <thaism.freire@outlook.com>
2021-06-16 17:41:24 -0300

    add filme -path profile

--- app/controllers/movie_picks_controller.rb
+++ app/controllers/movie_picks_controller.rb
@@ -1,5 +1,5 @@
 class MoviePicksController < ApplicationController
-  before_action :set_movie_pick, only: [:refuse, :add_to_watch_list, :remove_from_watch_list, :watch_movie]
+  before_action :set_movie_pick, only: [:refuse, :add_to_watch_list, :remove_from_watch_list, :watch_movie, :add_to_profile]
 
   def refuse
     @movie_pick.update(refused: true)
@@ -22,6 +22,11 @@ class MoviePicksController < ApplicationController
 
   def remove_from_watch_list
     watch_list
+    redirect_to profile_path
+  end
+
+  def add_to_profile
+    @movie_pick.update(watch_list: true)
     redirect_to profile_path
   end
 
